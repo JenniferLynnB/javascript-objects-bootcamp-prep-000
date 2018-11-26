adc71fce8e9421e1c188e66439f1170610d006ed
zenryoku <zenryoku@hotmail.com>
2018-11-26 04:11:10 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,6 +1,6 @@
 var playlist = new Object({artist: 'song'});
 
-function updatePlaylist(obj, artist, name) {
-  return Object.assign({}, obj, { [artist]: name});
+function updatePlaylist(playlist, artistName, songTitle) {
+  return Object.assign({}, playlist, { [artistName]: songTitle});
 }
 
